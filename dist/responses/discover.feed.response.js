d2af5226505783125cb11f4a1a303db49aac0e0e
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2021-03-17 18:22:04 +0300

    Build package

---
+++ dist/responses/discover.feed.response.js
@@ -0,0 +1,6 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const entities_1 = require('../entities');
+class DiscoverFeedResponseUser extends entities_1.ProfileEntity {}
+exports.DiscoverFeedResponseUser = DiscoverFeedResponseUser;
+//# sourceMappingURL=discover.feed.response.js.map
